200d4a122e27d9949156b0544cbb92cbe05ad102
Krutarth Chauhan <63405029+KITTU223@users.noreply.github.com>
2025-06-23 19:20:12 +0530

    all components are created

---
+++ src/components/Contact.jsx
@@ -0,0 +1,89 @@
+import React from "react";
+import { openingHours, socials } from "../../constants";
+import { useGSAP } from "@gsap/react";
+import { SplitText } from "gsap/all";
+import gsap from "gsap";
+
+const Contact = () => {
+  useGSAP(() => {
+    const titleSplit = SplitText.create("#contact h2", { type: "words" });
+    const timeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#contact",
+        start: "top center",
+      },
+      ease: "power1.inOut",
+    });
+
+    timeline
+      .from(titleSplit.words, {
+        opacity: 0,
+        yPercent: 100,
+        stagger: 0.02,
+      })
+      .from("#contact h3, #contact p", {
+        opacity: 0,
+        yPercent: 100,
+        stagger: 0.02,
+      })
+      .to("#f-right-leaf", {
+        y: "-50",
+        duration: 1,
+        ease: "power1.inOut",
+      })
+      .to(
+        "#f-left-leaf",
+        {
+          y: "-50",
+          duration: 1,
+          ease: "power1.inOut",
+        },
+        "<"
+      );
+  });
+  return (
+    <footer id="contact">
+      <img src="/images/footer-right-leaf.png" alt="r-leaf" id="f-right-leaf" />
+      <img src="/images/footer-left-leaf.png" alt="l-leaf" id="f-left-leaf" />
+
+      <div className="content">
+        <h2>Where to Find Us</h2>
+        <div>
+          <h3>Visit Our Bar</h3>
+          <p>718 Parker Rd, Boise, Ohio 99275</p>
+        </div>
+        <div>
+          <h3>Contact Us</h3>
+          <p>(368) 294-3614</p>
+          <p>demo@krutarthchauhan.shop</p>
+        </div>
+        <div>
+          <h3>Open Every Day</h3>
+          {openingHours.map((time) => (
+            <p key={time.day}>
+              {time.day} : {time.time}
+            </p>
+          ))}
+        </div>
+        <div>
+          <h3>Socials</h3>
+          <div className="flex-center gap-5">
+            {socials.map((soical) => (
+              <a
+                key={soical.name}
+                href={soical.url}
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label={soical.name}
+              >
+                <img src={soical.icon} alt="" />
+              </a>
+            ))}
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Contact;
